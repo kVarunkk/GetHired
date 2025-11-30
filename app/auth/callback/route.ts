aa3214ca2b9c37568526e7a88a00e7f4459dcaf8
kVarunkk <varunkumawatleap2@gmail.com>
2025-11-30 18:04:11 +0530

    fix: google sign up flow

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,25 +1,54 @@
 import { createClient } from "@/lib/supabase/server";
+import { SupabaseClient, User } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
-// The client you created from the Server-Side Auth instructions
+
+async function handleUserUpsert(user: User) {
+  const supabase = await createClient();
+  const { data: userData } = await supabase
+    .from("user_info")
+    .select("user_id")
+    .eq("user_id", user.id)
+    .limit(1);
+
+  if (!userData || userData.length === 0) {
+    console.log(`OAuth: Inserting new user_info row for ID: ${user.id}`);
+
+    const newUserInfo = {
+      user_id: user.id,
+      email: user.email,
+      is_job_digest_active: true,
+      is_promotion_active: true,
+    };
+
+    const { error: insertError } = await supabase
+      .from("user_info")
+      .insert(newUserInfo);
+
+    if (insertError) {
+      console.error(
+        "CRITICAL DB ERROR: Failed to insert new user_info row:",
+        insertError
+      );
+    }
+  }
+}
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   let next = searchParams.get("next") ?? "/";
   if (!next.startsWith("/")) {
-    // if "next" is not a relative URL, use the default
     next = "/";
   }
 
   if (code) {
     const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error && data.user) {
+      await handleUserUpsert(data.user);
+      const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
@@ -29,6 +58,5 @@ export async function GET(request: Request) {
     }
   }
 
-  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/error`);
 }
