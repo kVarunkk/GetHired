764b246e9fcd4aeea3620339165be403cc5161db
kVarunkk <varunkumawatleap2@gmail.com>
2025-12-25 02:24:59 +0530

    chore: update loading message

--- app/jobs/loading.tsx
+++ app/jobs/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   // Add fallback UI that will be shown while the route is loading.
   return (
     <div className="h-screen w-screen flex items-center justify-center text-muted-foreground">
-      Loading 3500+ Jobs...
+      Loading 2200+ Jobs...
     </div>
   );
 }
